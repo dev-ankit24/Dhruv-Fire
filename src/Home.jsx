81bcf13781ee64eff8056ccdc77d3fd3ca07262f
dev-ankit24 <ankitkumar839282@gmail.com>
2025-03-20 18:14:02 +0530

    create about

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Slider from './components/partials/Slider'
+import About from './components/partials/About'
 
 export default function Home() {
   return (
     <>
         <Slider/>
     <div className="container">
+        <div className="container-fluid">
+            <About/>
+        </div>
     </div>
     </>
   )
---
+++ src/components/partials/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+    <div className="about">
+        <h3 className='text-uppercase text-center mt-5'>Welcome to our Website</h3>
+        <p> <strong>JMD ENTERPRISES</strong> Fire Safety is a reputed organization engaged in manufacturing, installation and supplying a varied range of products that is inclusive of fire hydrant systems, fire Alarm system installation,fire extinguishers, hose pipes, fire brigade inlet connections, fire hydrant valves, fire alarm control panel, fire hose, fire hose delivery couplings, and more. Along with these products, we also offer services such as fire extinguisher refilling, fire extinguisher maintenance, fire fighting vehicle rental, and fire safety training. Our range is highly in demand and widely appreciated by our esteemed clients for sturdy construction, reliable performance, compact design, optimum performance, Flexible Sprinkler Local Hose, Tyco Flexible Sprinkler Hose,  dimensional accuracy, and longer functional life. We are capable of catering to the bulk requirements of the clients within the stipulated timeframe. <b className='text-danger'> Read More...</b></p>
+    </div>
+    </>
+  )
+}
